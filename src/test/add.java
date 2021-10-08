b483f6b70daf5b5882181658c500bc28f38f2491
Yuropatil <rohinidhanawade04@gmail.com>
2021-10-08 11:16:37 +0530

    along sum

--- src/test/add.java
+++ src/test/add.java
@@ -4,6 +4,11 @@ public class add {
 	public static void main(String[] args) {
 		int i=10,j=20;
 		System.out.println("Addition of two numbers="+(i+j));
+		int s=i+j;
+		if(s%2==0)
+		   System.out.println("Sum is Even ");
+		else 
+			System.out.println("Sum is odd");
 	}
 
 }
